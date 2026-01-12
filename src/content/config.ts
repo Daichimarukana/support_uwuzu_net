b400d96cd3448faa738acc3831dce6971b3e266d
だいちまる <98202777+Daichimarukana@users.noreply.github.com>
2026-01-13 02:08:14 +0900

    更新！！！！

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const docs = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    pubDate: z.date(),
+  }),
+});
+
+export const collections = { docs };
